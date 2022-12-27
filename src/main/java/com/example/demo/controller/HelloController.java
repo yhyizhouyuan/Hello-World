8163463b402e3c003f3a3df2ffe716a2823ea002
Galaxy <2017767351@qq.com>
2022-12-28 00:43:37 +0800

    test分支代码提交

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author Hance
+ * @version V1.0.0
+ * @date 2022/12/28 0:40
+ */
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
